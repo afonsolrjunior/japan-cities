7d612af5142d4b6c9deccebb60b82cfc81ddf599
Afonso Rodrigues <afonsorodrigues@Afonsos-Mac-mini.local>
2023-11-06 11:28:36 -0300

    Added JSON file with the initial cities

---
+++ japan-cities/japan-cities/Core/UI/Models/City.swift
@@ -0,0 +1,15 @@
+//
+//  City.swift
+//  japan-cities
+//
+//  Created by Afonso Rodrigues on 06/11/23.
+//
+
+import SwiftUI
+
+struct City: Identifiable {
+    let id: Int
+    let name: String
+    let description: String
+    let image: Image
+}
