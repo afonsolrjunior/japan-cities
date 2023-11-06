52618a72f2002f7adddf478028928569300aff3e
Afonso Rodrigues <afonsorodrigues@Afonsos-Mac-mini.local>
2023-11-06 10:33:53 -0300

    Finished initial setup

---
+++ japan-cities/japan-cities/Core/japan_citiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  japan_citiesApp.swift
+//  japan-cities
+//
+//  Created by Afonso Rodrigues on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct japan_citiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
